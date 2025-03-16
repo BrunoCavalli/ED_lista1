92d87f44c6dd22a5351c1d554e971d75e72ea193
unknown <brunocavalli12b@gmail.com>
2025-03-16 12:12:54 -0300

    add insertPatient

--- PatientArray.cpp
+++ PatientArray.cpp
@@ -21,9 +21,28 @@ void printPatients(PatientArray* pa) {
     std::cout << "\n Patients: \n" << pa->patients << std::endl;
 
     for (int i = 0; i < pa->size; i++) {
-        std::cout << "* " << pa->patients[i].arrival_time << std::endl;
-
+        std::cout << "* " << pa->patients[i].arrival_time
+        << " | " << pa->patients[i].severity
+        << " | " << pa->patients[i].name << std::endl;
     }
+    std::cout << std::endl;
 }
 
+void insertPatient(PatientArray *pa, Patient p) {
+    if (pa->size == pa->capacity) {
+        pa->capacity *= 2;
+        Patient* newPatients = new Patient[pa->capacity];
+
+        for (int i = 0; i < pa->size; i++) {
+            newPatients[i] = pa->patients[i];
+        }
+
+        delete[] pa->patients;
+        pa->patients = newPatients;
+    }
+    pa->patients[pa->size] = p;
+    pa->size++;
+}    
+
 }
+
