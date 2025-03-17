fd3bfad19e28e75e6bff765f6399f3b12741233f
unknown <brunocavalli12b@gmail.com>
2025-03-17 08:45:00 -0300

    implementei main

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project README
+
+## Project Name
+Listas - Lista 1
+
+## Description
+This project contains the first list of exercises for the Data structures course.
+
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "PatientArray.h"
+#include <iostream>
+
+int main() {
+    // Initialize the patient array
+    PatientArray::PatientArray* pa = PatientArray::initializePatientArray();
+
+    // Create some patients
+    PatientArray::Patient p1 = {"08:00", 1};
+    PatientArray::Patient p2 = {"08:30", 3};
+    PatientArray::Patient p3 = {"09:00", 4};
+    PatientArray::Patient p4 = {"09:30", 2};
+
+    // Insert patients into the array
+    PatientArray::insertPatient(pa, p1);
+    PatientArray::insertPatient(pa, p2);
+    PatientArray::insertPatient(pa, p3);
+    PatientArray::insertPatient(pa, p4);
+
+    // Print the patients
+    PatientArray::printPatients(pa);
+
+    // Find the next patient
+    int nextPatientIndex = PatientArray::findNextPatient(pa);
+    std::cout << "Next patient index: " << nextPatientIndex << std::endl;
+    std::cout << "Next patient: " << pa->patients[nextPatientIndex].name << std::endl;
+
+    // Remove a patient
+    PatientArray::removePatient(pa, nextPatientIndex);
+
+    // Print the patients again
+    PatientArray::printPatients(pa);
+
+    // Clean up
+    delete[] pa->patients;
+    delete pa;
+
+    return 0;
+}
